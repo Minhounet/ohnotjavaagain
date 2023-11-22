553472bdd467e3b73b1a85c49c5acfa4a3f700f3
QT6BCD4N <quang-minh-externe.tran@edf.fr>
2023-11-22 20:31:12 +0100

    Add module 3 solution

--- 03_interface/src/main/java/com/qmt/tutorial/fighter/Karateka.java
+++ 03_interface/src/main/java/com/qmt/tutorial/fighter/Karateka.java
@@ -1,15 +1,22 @@
 package com.qmt.tutorial.fighter;
 
+import java.util.Random;
+
 /**
  * Somebody who fights with bare hands
  */
-public class Karateka {
+public class Karateka implements Fighter {
+
+    private static final Random random = new Random();
+
 
     private final String specialAttack;
+
     private final Color kimonoColor;
     private final int strength;
     private final String name;
-    private final int hp;
+    private int hp;
+
 
     public Karateka(String name, String specialAttack, Color color, int hp, int strength) {
         this.name = name;
@@ -27,15 +34,28 @@ public class Karateka {
         return kimonoColor;
     }
 
-    public int getStrength() {
-        return strength;
-    }
-
+    @Override
     public String getName() {
         return name;
     }
 
-    public int getHp() {
+    @Override
+    public int getStrength() {
+        return 0;
+    }
+
+    @Override
+    public int getHP() {
         return hp;
+    }
+
+    @Override
+    public void decreaseHPBy(int damage) {
+        hp = Math.max(0, hp - damage);
+    }
+
+    @Override
+    public void attack(Fighter otherFighter) {
+        otherFighter.decreaseHPBy(random.nextInt(strength + 1));
     }
 }
--- 03_interface/src/main/java/com/qmt/tutorial/fighter/SwordMan.java
+++ 03_interface/src/main/java/com/qmt/tutorial/fighter/SwordMan.java
@@ -1,14 +1,19 @@
 package com.qmt.tutorial.fighter;
 
-/**
- * Be like Kenshin
- */
-public class SwordMan {
+import java.util.Random;
+
+public class SwordMan implements Fighter {
+
+    private static final Random random = new Random();
 
     private final String name;
+
     private final String swordName;
+
     private final int strength;
-    private final int hp;
+
+    private int hp;
+
 
     public SwordMan(String name, String swordName, int hp, int strength) {
         this.name = name;
@@ -21,12 +26,25 @@ public class SwordMan {
         return name;
     }
 
+    @Override
     public int getStrength() {
         return strength;
     }
 
-    public int getHp() {
+    @Override
+    public int getHP() {
         return hp;
+    }
+
+    @Override
+    public void decreaseHPBy(int damage) {
+        hp = Math.max(0, hp - damage);
+    }
+
+    @Override
+    public void attack(Fighter otherFighter) {
+        // Sword hurts really (+5)  but damage must not exceed strength
+        otherFighter.decreaseHPBy(Math.min(strength, random.nextInt(strength + 5)));
     }
 
     public String getSwordName() {
