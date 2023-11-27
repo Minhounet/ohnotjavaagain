6dbc86063c49f4945844a7392f48871381258db7
QT6BCD4N <quang-minh-externe.tran@edf.fr>
2023-11-27 11:57:20 +0100

    Correct LambdadaOfBrazil prior to sonar introspection

--- 05_lambda/src/main/java/com/qmt/tutorial/lambda/LambdadaOfBrazil.java
+++ 05_lambda/src/main/java/com/qmt/tutorial/lambda/LambdadaOfBrazil.java
@@ -8,13 +8,13 @@ import java.util.function.Function;
 public class LambdadaOfBrazil {
 
     // Write this function in lambda style
-    private static Function<String, Integer> countVowelsFunction = null;
+    private static final Function<String, Integer> COUNT_VOWELS_FUNCTION = null;
 
     public static int countVowelsWithoutLambdaKnowledge(String word, DunnoJavaLambda<String, Integer> countFunction) {
         return countFunction.apply(word);
     }
 
     public static int countVowels(String word) {
-        return countVowelsFunction.apply(word);
+        return COUNT_VOWELS_FUNCTION.apply(word);
     }
 }
